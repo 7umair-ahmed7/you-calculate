815bc132edc3276b93b23b145d55ca70d31d6cef
7Umair-Ahmed7 <7umairahmed7@gmail.com>
2025-07-14 11:50:56 +0500

    Logic added

---
+++ script.js
@@ -0,0 +1,25 @@
+let displayValueInput = document.querySelector(".display-value-input")
+let inputsContainer = document.querySelector(".inputs-container")
+
+
+ displayValueInput.disabled = true;
+inputsContainer.addEventListener("click", (e) => {
+
+    if (e.target.value == "=" || e.target.value == "AC" || e.target.value == "DE") {
+        if (e.target.value == "AC") {
+            displayValueInput.value = "";
+        }
+        if (e.target.value == "DE") {
+            console.log(displayValueInput.value.slice(0, -1))
+            let newVal = displayValueInput.value.slice(0, -1);
+            displayValueInput.value = newVal;
+        }
+        if (e.target.value == "=") {
+            displayValueInput.value = eval(displayValueInput.value)
+        }
+    }
+    else {
+        displayValueInput.value += e.target.value;
+
+    }
+})
